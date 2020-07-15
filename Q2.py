c175478c92b8f4334aa84bd568456dab6bb5a782
freshprinceofcoders <milanosolanki27@gmail.com>
2020-07-15 15:16:50 +0100

    doesnt work

--- Q2.py
+++ Q2.py
@@ -1,1 +1,14 @@
-
+sequance = [1,2]
+even_list = []
+def my_game(n):
+    num = 4000000
+    for n in range(num):
+        ans = sequance[-1] + sequance[-2]
+        sequance.append(ans)
+        #print(sequance)
+        if ans % 2 == 0 :
+            even_list.append(ans)
+            even_list.append(n)
+        answer = sum(even_list)
+        print(answer) 
+my_game('n')
