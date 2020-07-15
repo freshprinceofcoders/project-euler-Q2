0b9739f9fc655bfc9335c959bce44640bc73174a
freshprinceofcoders <milanosolanki27@gmail.com>
2020-07-15 16:33:25 +0100

    done

--- Q2.py
+++ Q2.py
@@ -1,14 +1,13 @@
-sequance = [1,2]
-even_list = []
-def my_game(n):
-    num = 4000000
-    for n in range(num):
-        ans = sequance[-1] + sequance[-2]
-        sequance.append(ans)
-        #print(sequance)
-        if ans % 2 == 0 :
-            even_list.append(ans)
-            even_list.append(n)
-        answer = sum(even_list)
-        print(answer) 
-my_game('n')
+
+num1 = 1
+num2 = 1
+n = 0
+res = 0
+while n < 4000000:
+    n = (num1 + num2)
+    if n % 2 == 0:
+        res = res + n
+    num1 = num2
+    num2 = n
+print(res)    
+
